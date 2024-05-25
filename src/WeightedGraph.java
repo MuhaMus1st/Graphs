a8cc5ad2190cfa5b9809f7aaf841c86fc15e53d0
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-25 22:17:58 +0500

    Update WeightedGraph.java

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,25 @@
+import java.util.*;
+
 public class WeightedGraph {
+    private final Map<Vertex, List<Edge>> adjacencyList = new HashMap<>();
+
+    public void addVertex(Vertex vertex) {
+        adjacencyList.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex source, Vertex destination, double weight) {
+        adjacencyList.get(source).add(new Edge(source, destination, weight));
+    }
+
+    public List<Edge> getEdges(Vertex vertex) {
+        return adjacencyList.get(vertex);
+    }
+
+    public Set<Vertex> getVertices() {
+        return adjacencyList.keySet();
+    }
+
+    public Map<Vertex, List<Edge>> getAdjacencyList() {
+        return adjacencyList;
+    }
 }
