574b77f5796ebf5747b7ffeae9563d5e21a7a352
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-25 22:16:30 +0500

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,26 @@
-public class BreadthFirstSearch {
+import java.util.*;
+
+public class BreadthFirstSearch extends Search {
+
+    public BreadthFirstSearch(WeightedGraph graph, Vertex start) {
+        super(graph, start);
+        bfs(start);
+    }
+
+    private void bfs(Vertex start) {
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(start);
+        visited.put(start, true);
+        while (!queue.isEmpty()) {
+            Vertex v = queue.poll();
+            for (Edge edge : graph.getEdges(v)) {
+                Vertex w = edge.getDestination();
+                if (!visited.getOrDefault(w, false)) {
+                    visited.put(w, true);
+                    edgeTo.put(w, v);
+                    queue.add(w);
+                }
+            }
+        }
+    }
 }
