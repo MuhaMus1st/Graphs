13126a2637077ebb5383de1662c6c1c688270710
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-25 22:17:25 +0500

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,29 @@
-public class Search {
+import java.util.*;
+
+public abstract class Search {
+    protected WeightedGraph graph;
+    protected Vertex start;
+    protected Map<Vertex, Boolean> visited;
+    protected Map<Vertex, Vertex> edgeTo;
+
+    public Search(WeightedGraph graph, Vertex start) {
+        this.graph = graph;
+        this.start = start;
+        visited = new HashMap<>();
+        edgeTo = new HashMap<>();
+    }
+
+    public boolean hasPathTo(Vertex v) {
+        return visited.getOrDefault(v, false);
+    }
+
+    public Iterable<Vertex> pathTo(Vertex v) {
+        if (!hasPathTo(v)) return null;
+        LinkedList<Vertex> path = new LinkedList<>();
+        for (Vertex x = v; x != start; x = edgeTo.get(x)) {
+            path.addFirst(x);
+        }
+        path.addFirst(start);
+        return path;
+    }
 }
