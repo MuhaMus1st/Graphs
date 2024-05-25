2c2f831187223bca94137439dec0957ea89e918e
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-25 22:17:44 +0500

    Update Vertex.java

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,29 @@
 public class Vertex {
+    private String id;
+
+    public Vertex(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vertex vertex = (Vertex) obj;
+        return id.equals(vertex.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
 }
