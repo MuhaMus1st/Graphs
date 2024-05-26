168f09ceff12ef2275251f34637bd10fd0329c82
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-26 22:03:20 +0500

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,26 +1,59 @@
+
 import java.util.*;
 
-public class BreadthFirstSearch extends Search {
+public class BreadthFirstSearch<V> {
+    private Set<Vertex<V>> marked;
+    private Map<Vertex<V>, Vertex<V>> edgeTo;
+    private Vertex<V> source;
 
-    public BreadthFirstSearch(WeightedGraph graph, Vertex start) {
-        super(graph, start);
-        bfs(start);
+    public BreadthFirstSearch(WeightedGraph<V> graph, V sourceData) {
+        this.marked = new HashSet<>();
+        this.edgeTo = new HashMap<>();
+        this.source = graph.getVertex(sourceData);
+        bfs(graph, this.source);
     }
 
-    private void bfs(Vertex start) {
-        Queue<Vertex> queue = new LinkedList<>();
-        queue.add(start);
-        visited.put(start, true);
+    private void bfs(WeightedGraph<V> graph, Vertex<V> source) {
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        queue.add(source);
+        marked.add(source);
+
         while (!queue.isEmpty()) {
-            Vertex v = queue.poll();
-            for (Edge edge : graph.getEdges(v)) {
-                Vertex w = edge.getDestination();
-                if (!visited.getOrDefault(w, false)) {
-                    visited.put(w, true);
+            Vertex<V> v = queue.poll();
+            for (Vertex<V> w : v.getAdjacentVertices().keySet()) {
+                if (!marked.contains(w)) {
                     edgeTo.put(w, v);
+                    marked.add(w);
                     queue.add(w);
                 }
             }
         }
+    }
+
+    public boolean hasPathTo(V destinationData) {
+        Vertex<V> destination = getVertexByData(destinationData);
+        return marked.contains(destination);
+    }
+
+    public Iterable<V> pathTo(V destinationData) {
+        if (!hasPathTo(destinationData)) return null;
+
+        Stack<V> path = new Stack<>();
+        Vertex<V> destination = getVertexByData(destinationData);
+
+        for (Vertex<V> x = destination; x != source; x = edgeTo.get(x)) {
+            path.push(x.getData());
+        }
+        path.push(source.getData());
+        return path;
+    }
+
+    private Vertex<V> getVertexByData(V data) {
+        for (Vertex<V> v : marked) {
+            if (v.getData().equals(data)) {
+                return v;
+            }
+        }
+        return null;
     }
 }
