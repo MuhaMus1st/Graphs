3d7f1409e5a0a2fee2c78765fdb05db3ef1a9909
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-26 22:04:30 +0500

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,28 +1,54 @@
-public class Edge {
-    private Vertex source;
-    private Vertex destination;
-    private double weight;
+import java.util.Objects;
 
-    public Edge(Vertex source, Vertex destination, double weight) {
+public class Edge<Vertex> {
+    private Vertex source;
+    private Vertex dest;
+    private Double weight;
+
+    public Edge(Vertex source, Vertex dest, Double weight) {
         this.source = source;
-        this.destination = destination;
+        this.dest = dest;
         this.weight = weight;
+    }
+
+    public Edge(Vertex source, Vertex dest) {
+        this.source = source;
+        this.dest = dest;
+    }
+
+    public void setSource(Vertex source) {
+        this.source = source;
     }
 
     public Vertex getSource() {
         return source;
     }
 
-    public Vertex getDestination() {
-        return destination;
+    public void setDest(Vertex dest) {
+        this.dest = dest;
     }
 
-    public double getWeight() {
+    public Vertex getDest() {
+        return dest;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
         return weight;
     }
 
     @Override
-    public String toString() {
-        return source + " -> " + destination + " : " + weight;
+    public boolean equals(Object o) {
+        if (this == o) return true; // references compared
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Edge<?> otherEdge = (Edge<?>) o;
+
+        return Objects.equals(this.source, otherEdge.source) &&
+                Objects.equals(this.dest, otherEdge.dest);
     }
 }
