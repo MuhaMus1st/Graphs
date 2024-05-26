813602ef61cf79b78d6b6b658f775ac7de5ddeba
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-05-26 22:06:36 +0500

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -1,29 +1,30 @@
 import java.util.*;
 
-public abstract class Search {
-    protected WeightedGraph graph;
-    protected Vertex start;
-    protected Map<Vertex, Boolean> visited;
+public class Search<Vertex> {
+    protected Set<Vertex> marked;
     protected Map<Vertex, Vertex> edgeTo;
+    protected final Vertex source;
 
-    public Search(WeightedGraph graph, Vertex start) {
-        this.graph = graph;
-        this.start = start;
-        visited = new HashMap<>();
+    public Search(Vertex source) {
+        this.source = source;
+        marked = new HashSet<>();
         edgeTo = new HashMap<>();
     }
 
     public boolean hasPathTo(Vertex v) {
-        return visited.getOrDefault(v, false);
+        return marked.contains(v);
     }
 
     public Iterable<Vertex> pathTo(Vertex v) {
         if (!hasPathTo(v)) return null;
-        LinkedList<Vertex> path = new LinkedList<>();
-        for (Vertex x = v; x != start; x = edgeTo.get(x)) {
-            path.addFirst(x);
+
+        LinkedList<Vertex> ls = new LinkedList<>();
+        for (Vertex i = v; i != source; i = edgeTo.get(i)) {
+            ls.push(i); // inverted adding
         }
-        path.addFirst(start);
-        return path;
+
+        ls.push(source);
+
+        return ls;
     }
 }
